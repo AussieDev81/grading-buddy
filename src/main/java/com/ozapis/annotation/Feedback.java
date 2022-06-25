5cb861a37cd6a70eda3276e4ed5881017cbbf663
AussieDev81 <aussiedev81@gmail.com>
2022-06-25 17:02:41 +1000

    javadoc comments

--- src/main/java/com/ozapis/annotation/Feedback.java
+++ src/main/java/com/ozapis/annotation/Feedback.java
@@ -3,10 +3,28 @@ package com.ozapis.annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * Annotate a class, method, or field with feedback metadata
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Feedback {
 
+    /**
+     * Provide a constructive feedback comment
+     * @return A comment about the annotated object
+     */
     String comment();
+
+    /**
+     * Give advice on this object
+     * @return Advice on this object as a string
+     */
     String advice() default "";
+
+    /**
+     * Share resources that may be helpful to the student.
+     * This could include URLs to website resources, or inspirational quotes or phrases etc
+     * @return An array of string resources
+     */
     String[] resources() default {};
 }
--- src/main/java/com/ozapis/annotation/Grade.java
+++ src/main/java/com/ozapis/annotation/Grade.java
@@ -3,9 +3,21 @@ package com.ozapis.annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * Provide details on the total marks available, and total marks awarded for the annotated item
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Grade {
 
+    /**
+     * Provide the total marks available for the annotated item
+     * @return A double value indicating the total marks available
+     */
     double marksAvailable() default 0;
+
+    /**
+     * Provide the marks awarded for the annotated item
+     * @return The total marks/grades awarded for this particular item
+     */
     double marksAwarded();
 }
